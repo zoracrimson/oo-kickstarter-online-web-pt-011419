2d690aa3d369e8917e6be7931dc1791c2f4f53a7
Kat Maldon <katharinamaldon@gmail.com>
2019-02-16 21:42:03 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+end
